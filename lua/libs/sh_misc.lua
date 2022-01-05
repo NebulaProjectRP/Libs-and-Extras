3c4e3902875778d6f65c5e2dc3a1867815d4c03a
Gonzalolog <gonzalolog@gmail.com>
2022-01-05 19:38:39 -0300

    Fixed merge

--- lua/libs/sh_misc.lua
+++ lua/libs/sh_misc.lua
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 NebulaAPI = "http://loopback.gmod:2069/";
 
-=======
->>>>>>> 68c68c1cb89564791cfbd51c7b1174a286c93096
 function p(x)
     return x and player.GetByID(x) or (CLIENT and LocalPlayer() or player.GetByID(1))
 end
