5df341ba9676a84c3e75e2464fffcd6395337f19
Wookie <bq1214@gmail.com>
2022-08-10 16:03:20 +0500

    Updated whitelist

--- lua/libs/sv_whitelist.lua
+++ lua/libs/sv_whitelist.lua
@@ -14,6 +14,7 @@ local ServerWhitelist = {
     "76561198031185743", -- Choicesquiddy
     "76561198877328209", -- Kibou
     "76561198369672715", -- MrDonDon
+    "76561198878066418", -- Obama Nae
 }
 
 hook.Add("CheckPassword", "Nebula.WhitelistCheck", function(steamid64)
