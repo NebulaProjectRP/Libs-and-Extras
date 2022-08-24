83a3c8bf6c2e3b8001300bc3b1361381b295ffea
Wookie <bq1214@gmail.com>
2022-08-24 19:58:33 +0500

    Added security module

--- lua/autorun/nebula_libload.lua
+++ lua/autorun/nebula_libload.lua
@@ -1,6 +1,23 @@
 MsgC(Color(255, 50, 255), "[LIB] ", color_white, " Loading Library utilities\n")
 
-for k, v in pairs(file.Find("libs/*.lua", "LUA")) do
+for _, v in pairs(file.Find("libs/*.lua", "LUA")) do
+    if (SERVER and string.StartWith(v, "sv_")) then
+        include("libs/" .. v)
+    elseif (string.StartWith(v, "cl_")) then
+        if SERVER then
+            AddCSLuaFile("libs/" .. v)
+        else
+            include("libs/" .. v)
+        end
+    elseif (string.StartWith(v, "sh_")) then
+        if SERVER then
+            AddCSLuaFile("libs/" .. v)
+        end
+        include("libs/" .. v)
+    end
+end
+
+for _, v in pairs(file.Find("libs/security/*.lua", "LUA")) do
     if (SERVER and string.StartWith(v, "sv_")) then
         include("libs/" .. v)
     elseif (string.StartWith(v, "cl_")) then
