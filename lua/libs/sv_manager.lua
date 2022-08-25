81f8d36617f1774fdda9a4300d07aeeb81d21a65
Gonzalolog <gonzalolog@gmail.com>
2022-08-25 05:10:21 -0300

    Added helper

--- lua/libs/sv_manager.lua
+++ lua/libs/sv_manager.lua
@@ -2,3 +2,7 @@ concommand.Add("reset", function(ply, cmd, args)
     if IsValid(ply) then return end
     RunConsoleCommand("changelevel", game.GetMap())
 end)
+
+function _gonzo()
+    return player.GetBySteamID64("76561198044940228")
+end
