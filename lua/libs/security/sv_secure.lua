8747d9c4435e62ef94d7401b4b5b0ceb661eb434
Wookie <bq1214@gmail.com>
2022-08-30 23:28:29 +0500

    Updated nebula secure

--- lua/libs/security/sv_secure.lua
+++ lua/libs/security/sv_secure.lua
@@ -1,7 +1,11 @@
 NebulaSecure = NebulaSecure or {}
 
 function NebulaSecure:Validate(steamid64, steamid32, ip)
-    local rawIp = string.Split(ip, ":")[1]
+    local info = self:Search(steamid64, steamid32, ip)
+
+    if not info then return true end
+
+    local rawIp = string.Split(info.ip, ":")[1]
 
     for _, ply in pairs(player.GetAll()) do
         if string.Split(ply:IPAddress(), ":")[1] == rawIp then
@@ -10,31 +14,37 @@ function NebulaSecure:Validate(steamid64, steamid32, ip)
         end
     end
 
-    if NebulaSecure.Players then
-        local found = false
-
-        for _, data in pairs(NebulaSecure.Players) do
-            -- if string.Split(ply.ip, ":")[1] == rawIp then
-            --     -- IP Warnings here
-            -- end
-
-            if data.steamid64 == steamid64 then found = true end
-        end
-
-        if not found then
-            local data = {
-                time = os.time(),
-                steamid64 = steamid64,
-                steamid32 = steamid32,
-                ip = ip,
-            }
-
-            NebulaDriver:MySQLInsert("secure_players", data, function()
-                table.insert(NebulaSecure.Players, data)
-                MsgN("[NebulaSecure] " .. steamid64 .. " has been added to the database.")
-            end)
-        end
-    end
+    -- for _, data in pairs(NebulaSecure.Players) do
+    --     -- More validation here
+    -- end
 
     return true
+end
+
+function NebulaSecure:Search(steamid64, steamid32, ip)
+    if not NebulaSecure.Players then return end
+
+    local found = false
+
+    for _, data in pairs(NebulaSecure.Players) do
+        if data.steamid64 == steamid64 then found = data end
+    end
+
+    if not found then
+        local data = {
+            time = os.time(),
+            steamid64 = steamid64,
+            steamid32 = steamid32,
+            ip = ip,
+        }
+
+        NebulaDriver:MySQLInsert("secure_players", data, function()
+            table.insert(NebulaSecure.Players, data)
+            MsgN("[NebulaSecure] " .. steamid64 .. " has been added to the database.")
+        end)
+
+        found = data
+    end
+
+    return found
 end
